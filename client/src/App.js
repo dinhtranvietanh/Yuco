75a0dc859adefe54b8fa2cb82b7a23f7d11a34ef
dinhtranvietanh <a7vavava@gmail.com>
2022-10-06 16:34:59 +0700

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import PageRender from './PageRender';
-
+import Home from './pages/home';
+import Login from './pages/login'
 import {
 
   Routes
@@ -15,6 +16,9 @@ function App() {
         <div className="main">
 
           <Routes>
+
+            <Route exact path="/" element={<Login />} />
+
             <Route exact path="/:page" element={<PageRender />} />
 
             <Route exact path="/:page/:id" element={<PageRender />} />
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+    const initialState = { email: '', password: '' }
+    const [userData, setUserData] = useState(initialState)
+    const { email, password } = userData
+
+    const handleChangeInput = e => {
+        const { name, value } = e.target
+        setUserData({ ...userData, [name]: value })
+    }
     return (
-        <div>
-            Login
+        <div className="auth_page">
+            <form>
+                <h3 className="text-uppercase">YuCo</h3>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" name="email"
+                        aria-describedby="emailHelp" onChange={handleChangeInput} value={email} />
+                    <small id="emailHelp" className="form-text text-muted">
+                        We'll never share your email with anyone else.
+                    </small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"
+                        onChange={handleChangeInput} value={password} name="password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary w-100"
+                    disabled={email && password ? false : true}>
+                    Login
+                </button>
+
+                <p className="my-2">
+                    Don't have an account? <Link to="/register" style={{ color: "crimson" }}> Register</Link>
+                </p>
+            </form>
         </div>
     )
 }
