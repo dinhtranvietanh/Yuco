a7f26a2d9c6ed602da2a755f7c4994caa939779f
dinhtranvietanh <57694670+dinhtranvietanh@users.noreply.github.com>
2022-11-10 09:37:41 +0700

    commentmodel

---
+++ models/commentModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const { likePost } = require('../controllers/postController')
+
+const comment_schema = new mongoose.Schema({
+
+
+
+    content: {
+        type: String, require: true
+    
+},
+
+likes:  [{type: mongoose.Types.ObjectId, ref: 'user'}],
+user: {type: mongoose.Types.ObjectId, ref: 'user'},
+reply: mongoose.Types.ObjectId,
+postID: mongoose.Types.ObjectId,
+postUserId: mongoose.Types.ObjectId
+
+}, {timestamp: true})
+
+module.exports = mongoose.model('comment',comment_schema
+)
